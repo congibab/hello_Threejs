7eecaa4efb30626b2be714e37f7ba488e8884fc7
congibab <engljapen2@gmail.com>
2021-07-06 00:35:50 +0900

    second commit

--- index.js
+++ index.js
@@ -1,50 +1,53 @@
-let camera, scene, renderer;
-
-init();
-GameLoop();
+ import * as THREE from "./build/three.module.js";
+ import { OrbitControls } from "./examples/jsm/controls/OrbitControls.js";
+ import { GLTFLoader } from "./examples/jsm/loaders/GLTFLoader.js";
+//import { GUI } from "https://threejs.org/examples/jsm/libs/dat.gui.module.js"
 
 function init() {
-    scene = new THREE.Scene();
+    const scene = new THREE.Scene();
+    const clock = new THREE.Clock();
 
-    camera = new THREE.PerspectiveCamera(
-        75,
-        window.innerWidth / window.innerHeight,
-        0.1,
-        1000
-    );
+    var delta = 0;
 
-    renderer = new THREE.WebGLRenderer();
 
+    const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+    const renderer = new THREE.WebGLRenderer();
     renderer.setSize(window.innerWidth, window.innerHeight);
-
     document.body.appendChild(renderer.domElement);
 
-    geometry = new THREE.BoxGeometry();
-    material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
-    cube = new THREE.Mesh(geometry, material);
+
+    const geometry = new THREE.BoxGeometry();
+
+    const uniforms = {
+        "time": { value: 1.0 },
+    };
+
+    const material = new THREE.ShaderMaterial({
+
+        uniforms: uniforms,
+        vertexShader: document.getElementById('vertexShader').textContent,
+        fragmentShader: document.getElementById('fragmentShader').textContent
+
+    });
+
+    const cube = new THREE.Mesh(geometry, material);
     scene.add(cube);
+
+    const controls = new OrbitControls(camera, renderer.domElement);
 
     camera.position.z = 5;
 
-    clock = new THREE.Clock();
-    delta = 0;
+    function animate() {
+        requestAnimationFrame(animate);
+
+        delta = clock.getDelta();
+        uniforms['time'].value = Math.sin(clock.getElapsedTime());
+        cube.rotation.x += delta;
+        cube.rotation.y += delta;
+
+        controls.update();
+        renderer.render(scene, camera);
+    };
+    animate();
 }
-
-function update() {
-
-    delta = clock.getDelta();
-    cube.rotation.x += 1 * delta;
-    cube.rotation.y += 1 * delta;
-    cube.material.color.setRGB(0, 1, 1);
-};
-
-function render() {
-    renderer.render(scene, camera);
-};
-
-function GameLoop() {
-    requestAnimationFrame(GameLoop);
-
-    update();
-    render();
-};
+init();
